6e4699b929464e82d11858f9835542b3a2f82730
dmitrydel <dmitrydel92@gmail.com>
2024-12-21 10:15:19 +0300

    Initial commit

---
+++ src/main/java/com/shendyuk/insurance_service_api/InsuranceServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.shendyuk.insurance_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shendyuk/insurance_service_api/InsuranceServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shendyuk.insurance_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
